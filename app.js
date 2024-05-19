c141f1266ee0cb84bca3087b35173b6004eb2439
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 08:36:38 +0700

    js lab 25

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+let score = prompt("enter yoour score ");
+if (score >= 80) {
+  alert(" A");
+} else if (score >= 70) {
+  alert(" B");
+} else if (score >= 60) {
+  alert(" C");
+} else if (score >= 50) {
+  alert(" D");
+} else {
+  alert(" F");
+}
